afbb7dcc6aad2d142dd4cea5666827415ec36a4e
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-29 12:52:20 +0530

    Add. Included testcase for subtract and multiplication operation

--- src/main/java/com/m2pfintech/ArithmeticOperation.java
+++ src/main/java/com/m2pfintech/ArithmeticOperation.java
@@ -1,6 +1,20 @@
 package com.m2pfintech;
 
-public class ArithmaticOperation
+public class ArithmeticOperation
 {
 
+    public int add(int firstNumber, int secondNumber)
+    {
+        return firstNumber+secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber )
+    {
+        return firstNumber-secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber)
+    {
+        return firstNumber*secondNumber;
+    }
 }
--- src/test/java/com/m2pfintech/ArithmeticOperationTest.java
+++ src/test/java/com/m2pfintech/ArithmeticOperationTest.java
@@ -1,6 +1,49 @@
 package com.m2pfintech;
 
-public class ArithmaticOperationTest
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArithmeticOperationTest
 {
-   ArithmaticOperation arithmaticObject = new ArithmaticOperation();
+   ArithmeticOperation arithmeticObject = new ArithmeticOperation();
+   @Test
+   void toReturnFourWhenOneIsAddedToThree()
+   {
+      int expectedValue = 4;
+
+      int actualValue = arithmeticObject.add(1,3);
+
+      assertEquals(expectedValue,actualValue);
+   }
+
+   @Test
+   void toReturnFourWhenFiveIsSubtractedFromNine()
+   {
+      int expectedValue = 4;
+
+      int actualValue = arithmeticObject.subtract(9,5);
+
+      assertEquals(expectedValue,actualValue);
+   }
+
+   @Test
+   void toReturnMinusFourWhenNineIsSubtractedFromFive()
+   {
+      int expectedValue = -4;
+
+      int actualValue = arithmeticObject.subtract(5,9);
+
+      assertEquals(expectedValue,actualValue);
+   }
+
+   @Test
+   void toReturnTenWhenFiveIsMultipliedByTwo()
+   {
+      int expectedValue = 10;
+
+      int actualValue = arithmeticObject.multiply(2,5);
+
+      assertEquals(expectedValue,actualValue);
+   }
 }
